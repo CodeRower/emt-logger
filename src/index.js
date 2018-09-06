54ae73ff1789206d3399d22e831441eeeb9c976c
CodeRower <owner.coderower@gmail.com>
2018-09-06 06:46:14 +0200

    Updated code to use as Module

--- src/index.js
+++ src/index.js
@@ -1,63 +1,71 @@
 const winston = require('winston');
 const fs = require('fs');
 const env = process.env.NODE_ENV || 'development';
-const logDir = 'log';
+const logDir = process.env.LOG_DIR || 'log';
+const logFile = process.env.LOG_FILE || 'log';
+
 // Create the log directory if it does not exist
 if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
+    fs.mkdirSync(logDir);
 }
 const tsFormat = () => (new Date()).toLocaleTimeString();
 const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-  transports: [
-    // colorize the output to the console
-    new (winston.transports.Console)({
-      timestamp: tsFormat,
-      colorize: true,
-      level: 'info'
-    }),
-    new (require('winston-daily-rotate-file'))({
-      filename: `${logDir}/reportingServer-%DATE%.log`,
-      datePattern: 'YYYY-MM-DD',
-      timestamp: tsFormat,
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '15d',
-      prepend: true,
-      level: env === 'development' ? 'verbose' : 'info'
-    })
-  ]
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        // colorize the output to the console
+        new (winston.transports.Console)({
+            timestamp: tsFormat,
+            colorize: true,
+            level: 'info'
+        }),
+        new (require('winston-daily-rotate-file'))({
+            filename: `${logDir}/${logFile}-%DATE%.log`,
+            datePattern: 'YYYY-MM-DD',
+            timestamp: tsFormat,
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '15d',
+            prepend: true,
+            level: env === 'development' ? 'verbose' : 'info'
+        })
+    ]
 });
 
-export function logError(file, method, sessionId, requestId, message) {
-  logger.error({
-    file: file,
-    method: method,
-    sessionId: sessionId,
-    requestId: requestId,
-    message: message
-  });
+function logError(file, method, sessionId, requestId, message) {
+    logger.error({
+        file: file,
+        method: method,
+        sessionId: sessionId,
+        requestId: requestId,
+        message: message
+    });
 }
 
-export function logInfo(file, method, sessionId, requestId, message) {
-  logger.info({
-    file: file,
-    method: method,
-    sessionId: sessionId,
-    requestId: requestId,
-    message: message
-  });
+function logInfo(file, method, sessionId, requestId, message) {
+    logger.info({
+        file: file,
+        method: method,
+        sessionId: sessionId,
+        requestId: requestId,
+        message: message
+    });
 }
 
-export function logVerbose(file, method, sessionId, requestId, message) {
-  logger.verbose({
-    file: file,
-    method: method,
-    sessionId: sessionId,
-    requestId: requestId,
-    message: message
-  });
+function logVerbose(file, method, sessionId, requestId, message) {
+    logger.verbose({
+        file: file,
+        method: method,
+        sessionId: sessionId,
+        requestId: requestId,
+        message: message
+    });
+}
+
+module.exports = {
+    logInfo: logInfo,
+    logError: logError,
+    logVerbose: logVerbose
 }
